22a6e4f295940c4e73702378d54e13c72ba0b94f
Nick Battle <nick.battle@gmail.com>
2016-06-21 16:22:59 +0100

    Correction for iota and multiple binds

--- FJ-VDMJ/src/main/java/org/overturetool/vdmj/syntax/BindReader.java
+++ FJ-VDMJ/src/main/java/org/overturetool/vdmj/syntax/BindReader.java
@@ -33,6 +33,7 @@ import org.overturetool.vdmj.lex.LexTokenReader;
 import org.overturetool.vdmj.lex.Token;
 import org.overturetool.vdmj.patterns.Bind;
 import org.overturetool.vdmj.patterns.MultipleBind;
+import org.overturetool.vdmj.patterns.MultipleSeqBind;
 import org.overturetool.vdmj.patterns.MultipleSetBind;
 import org.overturetool.vdmj.patterns.MultipleTypeBind;
 import org.overturetool.vdmj.patterns.Pattern;
@@ -195,11 +196,22 @@ public class BindReader extends SyntaxReader
 		switch (lastToken().type)
 		{
 			case IN:
-				if (nextToken().is(Token.SET))
+				nextToken();
+				
+				if (lastToken().is(Token.SET))
 				{
 					nextToken();
-					mb = new MultipleSetBind(
-							plist, getExpressionReader().readExpression());
+					mb = new MultipleSetBind(plist, getExpressionReader().readExpression());
+				}
+				else if (lastToken().is(Token.SEQ))
+				{
+					if (Settings.release == Release.CLASSIC)
+					{
+						throwMessage(2328, "Sequence binds are not available in classic");
+					}
+
+					nextToken();
+					mb = new MultipleSeqBind(plist, getExpressionReader().readExpression());
 				}
 				else
 				{
