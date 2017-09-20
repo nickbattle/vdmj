3217353ebed5d690a9f5288137954b4022fcb7c8
Nick Battle <nick.battle@gmail.com>
2017-09-20 16:51:47 +0100

    Added more methods to ValueFactory

--- FJ-VDMJ4/src/main/java/com/fujitsu/vdmj/values/ValueFactory.java
+++ FJ-VDMJ4/src/main/java/com/fujitsu/vdmj/values/ValueFactory.java
@@ -37,6 +37,76 @@ import com.fujitsu.vdmj.tc.types.TCType;
  */
 public class ValueFactory
 {
+	public static BooleanValue mkBool(boolean b)
+	{
+		return new BooleanValue(b);
+	}
+	
+	public static CharacterValue mkChar(char c)
+	{
+		return new CharacterValue(c);
+	}
+	
+	public static IntegerValue mkInt(long i)
+	{
+		return new IntegerValue(i);
+	}
+	
+	public static NaturalValue mkNat(long n) throws Exception
+	{
+		return new NaturalValue(n);
+	}
+	
+	public static NaturalOneValue mkNat1(long n) throws Exception
+	{
+		return new NaturalOneValue(n);
+	}
+	
+	public static RationalValue mkRat(long p, long q) throws Exception
+	{
+		return new RationalValue((double)p/q);
+	}
+
+	public static RationalValue mkRat(double d) throws Exception
+	{
+		return new RationalValue(d);
+	}
+
+	public static RealValue mkReal(double d) throws Exception
+	{
+		return new RealValue(d);
+	}
+
+	public static NilValue mkNil()
+	{
+		return new NilValue();
+	}
+	
+	public static QuoteValue mkQuote(String q)
+	{
+		return new QuoteValue(q);
+	}
+	
+	public static SeqValue mkSeq(Value ...args)
+	{
+		return new SeqValue(new ValueList(args));
+	}
+	
+	public static SetValue mkSet(Value ...args) throws ValueException
+	{
+		return new SetValue(new ValueSet(args));
+	}
+	
+	public static TupleValue mkTuple(Value ...args)
+	{
+		return new TupleValue(new ValueList(args));
+	}
+	
+	public static TokenValue mkToken(Value arg)
+	{
+		return new TokenValue(arg);
+	}
+	
 	public static RecordValue mkRecord(String module, String name, Value ...args) throws ValueException
 	{
 		TCType type = getType(module, name);
