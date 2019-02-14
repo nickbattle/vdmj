23e1c0807a0703b606553baf5a2692bfdd7617c6
nick_battle <nick.battle@gmail.com>
2019-02-14 18:11:19 +0000

    Added @Printf annotation

---
+++ Annotations/src/main/java/annotations/tc/TCPrintfAnnotation.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2018 Nick Battle.
+ *
+ *	Author: Nick Battle
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package annotations.tc;
+
+import com.fujitsu.vdmj.tc.annotations.TCAnnotation;
+import com.fujitsu.vdmj.tc.definitions.TCClassDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCDefinition;
+import com.fujitsu.vdmj.tc.expressions.TCExpression;
+import com.fujitsu.vdmj.tc.expressions.TCExpressionList;
+import com.fujitsu.vdmj.tc.expressions.TCStringLiteralExpression;
+import com.fujitsu.vdmj.tc.lex.TCIdentifierToken;
+import com.fujitsu.vdmj.tc.modules.TCModule;
+import com.fujitsu.vdmj.tc.statements.TCStatement;
+import com.fujitsu.vdmj.typechecker.Environment;
+import com.fujitsu.vdmj.typechecker.NameScope;
+
+public class TCPrintfAnnotation extends TCAnnotation
+{
+	public TCPrintfAnnotation(TCIdentifierToken name, TCExpressionList args)
+	{
+		super(name, args);
+	}
+
+	@Override
+	public void tcBefore(TCDefinition def, Environment env, NameScope scope)
+	{
+		name.report(6009, "@Printf only applies to statements and expressions");
+	}
+
+	@Override
+	public void tcBefore(TCModule module)
+	{
+		name.report(6009, "@Printf only applies to statements and expressions");
+	}
+
+	@Override
+	public void tcBefore(TCClassDefinition clazz)
+	{
+		name.report(6009, "@Printf only applies to statements and expressions");
+	}
+
+	@Override
+	public void tcBefore(TCExpression exp, Environment env, NameScope scope)
+	{
+		checkArgs(env, scope);
+	}
+
+	@Override
+	public void tcBefore(TCStatement stmt, Environment env, NameScope scope)
+	{
+		checkArgs(env, scope);
+	}
+
+	private void checkArgs(Environment env, NameScope scope)
+	{
+		if (args.isEmpty())
+		{
+			name.report(6008, "@Printf must srart with a string argument");
+		}
+		else
+		{
+			if (args.get(0) instanceof TCStringLiteralExpression)
+			{
+				for (TCExpression arg: args)
+				{
+					arg.typeCheck(env, null, scope, null);	// Just checks scope
+				}
+			}
+			else
+			{
+				name.report(6008, "@Printf must start with a string argument");
+			}
+		}
+	}
+}
