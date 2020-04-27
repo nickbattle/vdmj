1e4b6272179ae46ac96d4a43d65b4ec658a0eb8a
nick_battle <nick.battle@gmail.com>
2020-04-27 10:37:48 +0100

    Update lsp.sh for highprecision

--- lsp.sh
+++ lsp.sh
@@ -27,7 +27,7 @@ java -Dlog.filename=/dev/tty \
     -cp \
 $(latest "Annotations/target/annotations-1.0.0-*.jar"):\
 $(latest "Annotations2/target/annotations2-1.0.0-*.jar"):\
-$(latest "FJ-VDMJ4/target/vdmj-4.3.0-??????.jar"):\
+$(latest "FJ-VDMJ4/target/vdmj-4.3.0-P-??????.jar"):\
 $(latest "LSP/target/lsp-0.0.1-SNAPSHOT-*.jar") \
     lsp.LSPServerSocket $1 -lsp 8000 -dap 8001
 
