4e0f5d0a54f74c52f29828d653097d1adf5c8e52
nick_battle <nick.battle@gmail.com>
2020-06-04 17:15:26 +0100

    Run module/thread initialize in InitThread (for breakpoints)

---
+++ FJ-VDMJ4/src/main/java/com/fujitsu/vdmj/scheduler/InitThread.java
@@ -0,0 +1,337 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2016 Fujitsu Services Ltd.
+ *
+ *	Author: Nick Battle
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package com.fujitsu.vdmj.scheduler;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fujitsu.vdmj.Settings;
+import com.fujitsu.vdmj.debug.DebugLink;
+import com.fujitsu.vdmj.in.definitions.INClassDefinition;
+import com.fujitsu.vdmj.in.definitions.INClassList;
+import com.fujitsu.vdmj.in.modules.INModule;
+import com.fujitsu.vdmj.in.modules.INModuleList;
+import com.fujitsu.vdmj.lex.LexLocation;
+import com.fujitsu.vdmj.messages.Console;
+import com.fujitsu.vdmj.runtime.Context;
+import com.fujitsu.vdmj.runtime.ContextException;
+import com.fujitsu.vdmj.runtime.StateContext;
+import com.fujitsu.vdmj.tc.lex.TCIdentifierToken;
+import com.fujitsu.vdmj.tc.lex.TCNameToken;
+import com.fujitsu.vdmj.values.CPUValue;
+import com.fujitsu.vdmj.values.TransactionValue;
+
+/**
+ * A class representing the VDM initialization thread.
+ */
+public class InitThread extends SchedulableThread
+{
+	private static final long serialVersionUID = 1L;
+
+	private final INModuleList modules;
+	private final INClassList classes;
+	private final Context globalContext;
+	private Exception exception = null;
+
+
+	public InitThread(INModuleList modules, Context ctxt)
+	{
+		super(CPUResource.vCPU, null, 0, false, 0);
+
+		this.modules = modules;
+		this.classes = null;
+		this.globalContext = ctxt;
+		this.exception = null;
+
+		setName("InitThread");
+	}
+
+	public InitThread(INClassList classes, Context ctxt)
+	{
+		super(CPUResource.vCPU, null, 0, false, 0);
+
+		this.classes = classes;
+		this.modules = null;
+		this.globalContext = ctxt;
+		this.exception = null;
+
+		setName("InitThread");
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return (int)getId();
+	}
+
+	@Override
+	public synchronized void reschedule(Context ctxt, LexLocation location)
+	{
+		// ignore during initialize
+	}
+
+	@Override
+	public void body()
+	{
+		DebugLink link = DebugLink.getInstance();
+
+		try
+		{
+			if (modules != null)
+			{
+				initializeModules();
+			}
+			else if (classes != null)
+			{
+				initializeClasses();
+			}
+		}
+		catch (ContextException e)
+		{
+			setException(e);
+			suspendOthers();
+			
+			if (e.isStackOverflow())
+			{
+				e.ctxt.printStackFrames(Console.out);
+			}
+			
+			link.stopped(e.ctxt, e.location, e);
+		}
+		catch (Exception e)
+		{
+			while (e instanceof InvocationTargetException)
+			{
+				e = (Exception)e.getCause();
+			}
+			
+			setException(e);
+			suspendOthers();
+		}
+		finally
+		{
+			TransactionValue.commitAll();
+		}
+	}
+
+	public void initializeModules()
+	{
+		StateContext initialContext = (StateContext)globalContext;
+		initialContext.setThreadState(null);
+		Set<ContextException> problems = null;
+		Set<TCIdentifierToken> passed = new HashSet<TCIdentifierToken>();
+		int retries = 5;
+		boolean exceptions = Settings.exceptions;
+		Settings.exceptions = false;
+
+		do
+		{
+			problems = new HashSet<ContextException>();
+
+        	for (INModule m: modules)
+    		{
+        		if (passed.contains(m.name))
+        		{
+        			continue;
+        		}
+
+        		long before = System.currentTimeMillis();
+        		Set<ContextException> e = m.initialize(initialContext);
+        		long after = System.currentTimeMillis();
+        		
+        		if (Settings.verbose && (after-before) > 200)
+        		{
+        			Console.out.printf("Pass %d: %s = %.3f secs\n", (6-retries), m.name, (double)(after-before)/1000);
+        		}
+
+        		if (e != null)
+        		{
+        			problems.addAll(e);
+
+        			if (e.size() == 1 && e.iterator().next().isStackOverflow())
+        			{
+        				retries = 0;
+        				break;
+        			}
+        		}
+        		else
+        		{
+        			passed.add(m.name);
+        		}
+     		}
+        	
+        	if (Settings.verbose && !problems.isEmpty())
+        	{
+        		Console.out.printf("Pass %d:\n", (6-retries));
+
+    			for (ContextException e: problems)
+    			{
+    				Console.out.println(e.toString());
+    			}        		
+        	}
+		}
+		while (--retries > 0 && !problems.isEmpty());
+
+		if (!problems.isEmpty())
+		{
+			ContextException toThrow = problems.iterator().next();
+
+			for (ContextException e: problems)
+			{
+				Console.err.println(e.toString());
+
+				if (e.number != 4034)	// Not in scope err
+				{
+					toThrow = e;
+				}
+			}
+
+			throw toThrow;
+		}
+
+		Settings.exceptions = exceptions;
+	}
+
+	public void initializeClasses()
+	{
+		globalContext.setThreadState(CPUValue.vCPU);
+
+		// Initialize all the functions/operations first because the values
+		// "statics" can call them.
+
+		for (INClassDefinition cdef: classes)
+		{
+			cdef.staticInit(globalContext);
+		}
+
+		// Values can forward reference each other, which means that we don't
+		// know what order to initialize the classes in. So we have a crude
+		// retry mechanism, looking for "forward reference" like exceptions.
+
+		ContextException failed = null;
+		int retries = 3;	// Potentially not enough.
+		Set<ContextException> trouble = new HashSet<ContextException>();
+		Set<TCNameToken> passed = new HashSet<TCNameToken>();
+		boolean exceptions = Settings.exceptions;
+		Settings.exceptions = false;
+
+		do
+		{
+			failed = null;
+			trouble.clear();
+
+    		for (INClassDefinition cdef: classes)
+    		{
+				if (passed.contains(cdef.name))
+				{
+					continue;
+				}
+
+    			long before = System.currentTimeMillis();
+    			long after;
+
+    			try
+    			{
+            		cdef.staticValuesInit(globalContext);
+    				passed.add(cdef.name);
+    			}
+    			catch (ContextException e)
+    			{
+    				if (e.isStackOverflow())
+    				{
+    					trouble.clear();
+    					trouble.add(e);
+    					retries = 0;
+    					break;
+    				}
+    				
+    				trouble.add(e);
+    				
+    				// These two exceptions mean that a member could not be
+    				// found, which may be a forward reference, so we retry...
+
+    				if (e.number == 4034 || e.number == 6)
+    				{
+    					failed = e;
+    				}
+    				else
+    				{
+    					throw e;
+    				}
+    			}
+    			finally
+    			{
+    				after = System.currentTimeMillis();
+    				
+            		if (Settings.verbose && (after-before) > 200)
+            		{
+            			Console.out.printf("Pass %d: %s = %.3f secs\n", (4-retries), cdef.name.getName(), (double)(after-before)/1000);
+            		}
+    			}
+    		}
+    		
+        	if (Settings.verbose && !trouble.isEmpty())
+        	{
+        		Console.out.printf("Pass %d:\n", (4-retries));
+
+    			for (ContextException e: trouble)
+    			{
+    				Console.out.println(e.toString());
+    			}        		
+        	}
+		}
+		while (--retries > 0 && failed != null);
+
+		if (!trouble.isEmpty())
+		{
+			ContextException toThrow = trouble.iterator().next();
+
+			for (ContextException e: trouble)
+			{
+				Console.err.println(e.toString());
+
+				if (e.number != 4034)	// Not in scope err
+				{
+					toThrow = e;
+				}
+			}
+
+			throw toThrow;
+		}
+
+		Settings.exceptions = exceptions;
+	}
+
+	public void setException(Exception e)
+	{
+		Console.err.println(e.getMessage());
+		exception = e;
+	}
+	
+	public Exception getException()
+	{
+		return exception;
+	}
+}
