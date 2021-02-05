0c352cfc18d44b0fb9f3e47607958d48a429b80c
nick_battle <nick.battle@gmail.com>
2021-02-05 10:20:24 +0000

    Process nested block comments

--- vdmj/src/main/java/com/fujitsu/vdmj/lex/LexTokenReader.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/lex/LexTokenReader.java
@@ -859,10 +859,11 @@ public class LexTokenReader extends BacktrackInputReader
 					StringBuilder sb = new StringBuilder();
 					rdCh();
 					LexLocation here = location(linecount, charpos);
+					int nestedCount = 0;
 					
 					while (ch != EOF)
 					{
-					    while (ch != '*' && ch != EOF)
+					    while (ch != '*' && ch != '/' && ch != EOF)
 					    {
 					    	sb.append(ch);
 					    	rdCh();
@@ -872,11 +873,32 @@ public class LexTokenReader extends BacktrackInputReader
 					    {
 					    	throwMessage(1011, tokline, tokpos, "Unterminated block comment");
 					    }
+					    else if (ch == '/')
+					    {
+					    	sb.append('/');
+
+					    	if (rdCh() == '*')
+						    {
+						    	nestedCount++;
+						    	sb.append('*');
+						    	rdCh();
+						    }
+					    }
 					    else
 					    {
 						    if (rdCh() == '/')
 						    {
-						    	break;
+						    	if (nestedCount == 0)
+						    	{
+						    		break;
+						    	}
+						    	else
+						    	{
+						    		nestedCount--;
+						    		sb.append('*');
+						    		sb.append('/');
+						    		rdCh();
+						    	}
 						    }
 						    else
 						    {
