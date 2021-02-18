b3004ecd127fd377d33675ba2f884e710a33009b
nick_battle <nick.battle@gmail.com>
2021-02-18 17:12:37 +0000

    Adjusted order plugin (again)

--- cmd-plugins/src/main/java/plugins/OrderPlugin.java
+++ cmd-plugins/src/main/java/plugins/OrderPlugin.java
@@ -28,7 +28,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -48,6 +47,8 @@ import com.fujitsu.vdmj.typechecker.FlatEnvironment;
 
 public class OrderPlugin extends CommandPlugin
 {
+	private List<String> startpoints = new Vector<String>();
+	
 	public OrderPlugin(Interpreter interpreter)
 	{
 		super(interpreter);
@@ -56,10 +57,11 @@ public class OrderPlugin extends CommandPlugin
 	@Override
 	public boolean run(String[] argv) throws Exception
 	{
-		if (argv.length != 1)
+		startpoints.clear();
+		
+		for (int i=1; i < argv.length; i++)
 		{
-			Console.out.println(help());
-			return true;
+			startpoints.add(argv[i]);
 		}
 
 		if (interpreter instanceof ModuleInterpreter)
@@ -90,9 +92,10 @@ public class OrderPlugin extends CommandPlugin
 			}
 		}
 
-		Map<String, Set<String>> dependencies = new HashMap<String, Set<String>>();
+		Map<String, Set<String>> uses = new HashMap<String, Set<String>>();
+		Map<String, Set<String>> usedBy  = new HashMap<String, Set<String>>();
 		Environment globals = new FlatEnvironment(alldefs, null);
-		List<String> startpoints = new Vector<String>();
+		Set<String> allModules = new HashSet<String>();
 
 		for (TCModule m: modules)
 		{
@@ -104,85 +107,107 @@ public class OrderPlugin extends CommandPlugin
 				freevars.addAll(def.getFreeVariables(globals, empty, new AtomicBoolean(false)));
 	    	}
 	    	
-	    	Set<String> modnames = new HashSet<String>();
+	    	Set<String> depnames = new HashSet<String>();
 	    	String myname = m.name.getName();
+	    	allModules.add(myname);
 	    	
 	    	for (TCNameToken name: freevars)
 	    	{
-	    		modnames.add(name.getModule());
+	    		depnames.add(name.getModule());		// Just module names
 	    	}
 	    	
-	    	dependencies.put(myname, modnames);
+	    	uses.put(myname, depnames);
 	    	
-	    	if (modnames.isEmpty() || modnames.size() == 1 && modnames.contains(myname))
+	    	for (String depname: depnames)
 	    	{
-	    		startpoints.add(myname);
+	    		Set<String> curr = usedBy.get(depname);
+	    		
+	    		if (curr == null)
+	    		{
+	    			curr = new HashSet<String>();
+	    			usedBy.put(depname, curr);
+	    		}
+	    		
+	    		curr.add(myname);
 	    	}
 		}
-		
-		if (startpoints.isEmpty())	// complex loops, so choose smallest dependencies
+
+		if (startpoints.isEmpty())
 		{
-			int min = Integer.MAX_VALUE;
-			
-			for (String name: dependencies.keySet())
+			for (TCModule m: modules)
 			{
-				int size = dependencies.get(name).size();
+				String name = m.name.getName();
 				
-				if (size < min)
-				{
-					min = size;
-					startpoints.clear();
-					startpoints.add(name);
-				}
-				else if (size == min)
+				if (usedBy.get(name) == null)
 				{
 					startpoints.add(name);
 				}
 			}
+			
+			if (startpoints.isEmpty())
+			{
+				Console.out.println("Module dependencies have cycles");
+				Console.out.println("Run 'order <start modules>'");
+				Console.out.println("Where start modules are the least dependent.");
+				return;
+			}
+		}
+		else
+		{
+			boolean failed = false;
+			
+			for (String name: startpoints)
+			{
+				if (!allModules.contains(name))
+				{
+					Console.out.println("Unknown module: " + name);
+					failed = true;
+				}
+			}
+			
+			if (failed)
+			{
+				return;
+			}
 		}
 		
-		// This is Kuhn's algorithm for topological sorting (but allowing loops)
-	
-		List<String> ordering = new Vector<String>(dependencies.size());
+		List<String> ordering = new Vector<String>();
 		
 		while (!startpoints.isEmpty())
 		{
 		    String node = startpoints.remove(0);
-		    
-		    if (!ordering.contains(node))
-		    {
-		    	ordering.add(node);
-		    }
-		    
-			for (Entry<String, Set<String>> pair: dependencies.entrySet())
-		    {
-				String key = pair.getKey();
-				Set<String> deps = pair.getValue();
-				
-				if (deps.contains(node))
-				{
-					deps.remove(node);
-					
-					if (deps.isEmpty() || deps.size() == 1 && deps.contains(key))
+	    	ordering.add(node);
+	    	Set<String> usedBySet = usedBy.get(node);
+	    	
+	    	if (usedBySet != null)
+	    	{
+		    	for (String next: usedBySet)
+		    	{
+					if (!ordering.contains(next) && !startpoints.contains(next))
 					{
-						if (!startpoints.contains(key))
-						{
-							startpoints.add(key);
-						}
+						startpoints.add(next);
 					}
-				}
-		    }
+			    }
+	    	}
 		}
 		
 		for (String name: ordering)
 		{
 			Console.out.println(name);
 		}
+		
+		for (String name: allModules)
+		{
+			if (!ordering.contains(name))
+			{
+				Console.out.println("Missing: " + name);
+			}
+		}
     }
 
 	@Override
 	public String help()
 	{
-		return "order - print optimal module/class order";
+		return "order [start points] - print optimal module/class order";
 	}
 }
