cb730da6743995d38d48612f2bf494d21a56be79
nick_battle <nick.battle@gmail.com>
2021-05-16 14:02:09 +0100

    Omit long precision in rdNumber for high precision

--- vdmj/src/main/java/com/fujitsu/vdmj/lex/LexTokenReader.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/lex/LexTokenReader.java
@@ -538,7 +538,7 @@ public class LexTokenReader extends BacktrackInputReader
 				try
 				{
 					String s = v.toString();
-					Long.parseLong(s);	// Just check
+					// Long.parseLong(s);	// Not checked for High Precision
 					return s;
 				}
 				catch (NumberFormatException e)
