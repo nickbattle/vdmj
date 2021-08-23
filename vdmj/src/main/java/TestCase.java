5d06c7e30273b2f2bd8c602346ddb43c25228020
nick_battle <nick.battle@gmail.com>
2021-08-23 12:18:59 +0100

    Imported VDMUnit support from Overture

---
+++ vdmj/src/main/java/TestCase.java
@@ -0,0 +1,193 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2016 Aarhus University.
+ *
+ *	Author: Nick Battle and Kenneth Lausdahl
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *	SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ ******************************************************************************/
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.fujitsu.vdmj.messages.ConsolePrintWriter;
+import com.fujitsu.vdmj.runtime.ClassInterpreter;
+import com.fujitsu.vdmj.runtime.Context;
+import com.fujitsu.vdmj.runtime.ExitException;
+import com.fujitsu.vdmj.runtime.StateContext;
+import com.fujitsu.vdmj.values.NameValuePair;
+import com.fujitsu.vdmj.values.ObjectValue;
+import com.fujitsu.vdmj.values.OperationValue;
+import com.fujitsu.vdmj.values.Value;
+import com.fujitsu.vdmj.values.ValueList;
+import com.fujitsu.vdmj.values.VoidValue;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.*;
+import java.io.*;
+
+
+public class TestCase {
+    private final static String xmlReportTemplate = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t";
+    private final static String xmlReportSuiteTemplate = "<testsuite xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"https://maven.apache.org/surefire/maven-surefire-plugin/xsd/surefire-test-report.xsd\" name=\"%s\" time=\"0.0\" tests=\"0\" errors=\"0\" skipped=\"0\" failures=\"0\"/>";
+    private final static String vdmUnitReportEnable = "vdm.unit.report";
+
+    public static Value reflectionRunTest(Value obj, Value name)
+            throws Exception {
+        String methodName = name.toString().replaceAll("\"", "").trim();
+
+        ObjectValue instance = (ObjectValue) obj;
+        for (NameValuePair p : instance.members.asList()) {
+            if (p.name.getName().equals(methodName)) {
+                if (p.value instanceof OperationValue) {
+                    OperationValue opVal = (OperationValue) p.value;
+                    Context mainContext = new StateContext(p.name.getLocation(), "reflection scope");
+
+                    mainContext.putAll(ClassInterpreter.getInstance().getInitialContext());
+                    // mainContext.putAll(ClassInterpreter.getInstance().);
+                    mainContext.setThreadState(ClassInterpreter.getInstance().getInitialContext().threadState.CPU);
+
+                    long timerStart = System.nanoTime();
+                    boolean success = false;
+                    ExitException error = null;
+                    try {
+                        opVal.eval(p.name.getLocation(), new ValueList(), mainContext);
+                        success = true;
+                    } catch (Exception e) {
+                        if (e instanceof ExitException) {
+                            if(((ExitException)e).value.objectValue(null).type.name.getName().equals("AssertionFailedError"))
+                            {
+                                success = false;
+                            }
+                            throw e;
+                        }
+
+                        try {
+                            return  ClassInterpreter.getInstance().evaluate("Error`throw(\""
+                                    + e.getMessage().replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\\\"").replaceAll("\'", "\\\'")
+                                    + "\")", mainContext);
+                        }catch(ExitException e2)
+                        {
+                            error = e2;
+                            throw e2;
+                        }
+                    } finally {
+                        long totalExecTime = System.nanoTime() - timerStart;
+
+                        if (System.getProperty(vdmUnitReportEnable) != null) {
+
+                            String containerName = "";
+
+                            if (obj instanceof ObjectValue) {
+                                containerName = ((ObjectValue) obj).type.name.getName();
+                            }
+
+                            recordTestResults(containerName, methodName, success, error, totalExecTime);
+                        }
+                    }
+                }
+            }
+        }
+        return new VoidValue();
+
+    }
+
+    private static void recordTestResults(String containerName, String methodName, boolean success, ExitException error, long totalExecTime) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException {
+
+        File report = new File("TEST-" + containerName + ".xml");
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = null;
+
+        if (report.exists()) {
+            doc = docBuilder.parse(report);
+
+        } else {
+            doc = docBuilder.parse(new ByteArrayInputStream(String.format(xmlReportTemplate + xmlReportSuiteTemplate, containerName).getBytes()));
+        }
+
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+        XPathExpression expr = xpath.compile("/testsuite[@name='" + containerName + "']");
+
+        NodeList nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+        if (nl.getLength() > 0) {
+            Element testSuiteNode = (Element) nl.item(0);
+
+            expr = xpath.compile("testcase[@name='" + methodName + "']");
+            NodeList nlT = (NodeList) expr.evaluate(testSuiteNode, XPathConstants.NODESET);
+            Element n = null;
+
+            if (nlT.getLength() > 0) {
+                n = (Element) nlT.item(0);
+            } else {
+                n = doc.createElement("testcase");
+            }
+
+            while(n.getFirstChild()!=null)
+            {
+                n.removeChild(n.getFirstChild());
+            }
+
+            n.setAttribute("name", methodName);
+            n.setAttribute("classname", containerName);
+            n.setAttribute("time", totalExecTime * 1E-9 + "");
+            testSuiteNode.appendChild(n);
+
+            testSuiteNode.setAttribute("tests", String.valueOf(Integer.parseInt(testSuiteNode.getAttribute("tests")) + 1));
+
+            if (error!=null) {
+                testSuiteNode.setAttribute("error", String.valueOf(Integer.parseInt(testSuiteNode.getAttribute("errors")) + 1));
+                Element errorElement = doc.createElement("error");
+                errorElement.setAttribute("message",error.number+"");
+                errorElement.setAttribute("type","ERROR");
+                PrintWriter strOut = new PrintWriter(new StringWriter());
+                error.ctxt.printStackTrace(new ConsolePrintWriter(strOut),true);
+                errorElement.setTextContent(strOut.toString());
+                n.appendChild(errorElement);
+            } else if (!success) {
+                testSuiteNode.setAttribute("failures", String.valueOf(Integer.parseInt(testSuiteNode.getAttribute("failures")) + 1));
+                Element failureElement = doc.createElement("failure");
+                failureElement.setAttribute("message",methodName);
+                failureElement.setAttribute("type","WARNING");
+                failureElement.setAttribute("time", totalExecTime * 1E-9 + "");
+                n.appendChild(failureElement);
+            }
+
+            testSuiteNode.setAttribute("time", String.valueOf(Double.parseDouble(testSuiteNode.getAttribute("time")) + (totalExecTime * 1E-9)));
+
+        }
+
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        Result output = new StreamResult(report);
+        Source input = new DOMSource(doc);
+
+        transformer.transform(input, output);
+
+
+    }
+}
---
+++ vdmj/src/main/java/TestSuite.java
@@ -0,0 +1,198 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2016 Aarhus University.
+ *
+ *	Author: Nick Battle and Kenneth Lausdahl
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *	SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ ******************************************************************************/
+
+import java.util.List;
+import java.util.Vector;
+
+import com.fujitsu.vdmj.in.definitions.INExplicitOperationDefinition;
+import com.fujitsu.vdmj.lex.Token;
+import com.fujitsu.vdmj.runtime.ClassInterpreter;
+import com.fujitsu.vdmj.runtime.Context;
+import com.fujitsu.vdmj.runtime.ObjectContext;
+import com.fujitsu.vdmj.runtime.StateContext;
+import com.fujitsu.vdmj.tc.definitions.TCDefinition;
+import com.fujitsu.vdmj.tc.types.TCOperationType;
+import com.fujitsu.vdmj.values.NameValuePair;
+import com.fujitsu.vdmj.values.ObjectValue;
+import com.fujitsu.vdmj.values.OperationValue;
+import com.fujitsu.vdmj.values.SeqValue;
+import com.fujitsu.vdmj.values.Value;
+import com.fujitsu.vdmj.values.ValueList;
+
+public class TestSuite
+{
+	public static Value getTestMethodNamed(Value test)
+	{
+		List<String> tests = new Vector<String>();
+		ObjectValue instance = (ObjectValue) test;
+
+		for (NameValuePair p : instance.members.asList())
+		{
+			if (p.value instanceof OperationValue)
+			{
+				if (p.name.getName().toLowerCase().startsWith("test"))
+				{
+					tests.add(p.name.getName());
+				}
+			}
+		}
+
+		ValueList vals = new ValueList();
+		for (String value : tests)
+		{
+			vals.add(new SeqValue(value));
+		}
+
+		return new SeqValue(vals);
+	}
+
+	public static Value createTests(Value test) throws Exception
+	{
+		List<String> tests = new Vector<String>();
+		ValueList vals = new ValueList();
+		ObjectValue instance = (ObjectValue) test;
+
+		for (NameValuePair p : instance.members.asList())
+		{
+			if (p.value instanceof OperationValue)
+			{
+				OperationValue opVal = (OperationValue) p.value;
+				if (!opVal.isConstructor
+						&& p.name.getName().toLowerCase().startsWith("test"))
+				{
+					tests.add(p.name.getName());
+
+					Context mainContext = new StateContext(p.name.getLocation(), "reflection scope");
+
+					mainContext.putAll(ClassInterpreter.getInstance().getInitialContext());
+					mainContext.setThreadState(ClassInterpreter.getInstance().getInitialContext().threadState.CPU);
+
+					try
+					{
+						INExplicitOperationDefinition ctor = getTestConstructor(instance);
+						if (ctor == null
+								|| !ctor.name.getModule().equals(instance.type.name.getLocation().module)
+								&& ctor.parameterPatterns.isEmpty()
+								|| ctor.accessSpecifier.access == Token.PUBLIC)
+						{
+							throw new Exception("Class "
+									+ p.name.getModule()
+									+ " has no public constructor TestCase(String name) or TestCase()");
+						}
+
+						String vdmTestExpression = "";
+						if (((TCOperationType) ctor.getType()).parameters.size() == 1)
+						{
+							vdmTestExpression = "new " + p.name.getModule()
+									+ "(\"" + p.name.getName() + "\")";
+							vals.add(ClassInterpreter.getInstance().evaluate(vdmTestExpression, mainContext));
+						} else
+						{
+							boolean foundSetName = false;
+							// check that we have setName and that it is accesiable
+							for (TCDefinition def : instance.type.definitions)
+							{
+								if (def.name.getName().equals("setName"))
+								{
+									foundSetName = true;
+								}
+							}
+							if (!foundSetName)
+							{
+								throw new Exception("Cannot instantiate test case: "
+										+ instance.type.name.getName());
+							}
+
+							vdmTestExpression = "new " + p.name.getModule()
+									+ "()";
+							Value testClassInstance = ClassInterpreter.getInstance().evaluate(vdmTestExpression, mainContext);
+							ObjectValue tmp = (ObjectValue) testClassInstance;
+							ObjectContext octxt = new ObjectContext(mainContext.location, "TestClassContext", mainContext, tmp);
+							vdmTestExpression = "setName(\"" + p.name.getName()
+									+ "\")";
+
+							ClassInterpreter.getInstance().evaluate(vdmTestExpression, octxt);
+							vals.add(testClassInstance);
+						}
+
+					} catch (Exception e)
+					{
+						throw e;
+					}
+				}
+			}
+		}
+		return new SeqValue(vals);
+	}
+
+	private static INExplicitOperationDefinition getTestConstructor(
+			ObjectValue instance)
+	{
+		INExplicitOperationDefinition ctor = getConstructor(instance, "seq of (char)");
+		if (ctor != null)
+		{
+			return ctor;
+		}
+		return getConstructor(instance, "()");
+	}
+
+	private static INExplicitOperationDefinition getConstructor(
+			ObjectValue instance, String typeName)
+	{
+		boolean searchForDefaultCtor = typeName.trim().equals("()");
+		INExplicitOperationDefinition defaultSuperCtor = null;
+		for (NameValuePair pair : instance.members.asList())
+		{
+			if (pair.value instanceof OperationValue)
+			{
+				OperationValue op = (OperationValue) pair.value;
+				if (op.isConstructor)
+				{
+					if (searchForDefaultCtor
+							&& ((TCOperationType) op.expldef.getType()).parameters.isEmpty())
+					{
+						if (op.expldef.name.getName().equals(instance.type.name.getName()))
+						{
+							return op.expldef;
+						} else
+						{
+							defaultSuperCtor = op.expldef;
+						}
+					} else if (((TCOperationType) op.expldef.getType()).parameters.size() == 1
+							&& ((TCOperationType) op.expldef.getType()).parameters.get(0).toString().equals(typeName)
+							&& op.expldef.name.getName().equals(instance.type.name.getName()))
+					{
+						return op.expldef;
+					}
+				}
+			}
+		}
+
+		if (searchForDefaultCtor)
+		{
+			return defaultSuperCtor;
+		}
+		return null;
+	}
+}
