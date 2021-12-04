6bd1b106e871b6257faaa17d9d6d5198b9cebfd5
nick_battle <nick.battle@gmail.com>
2021-12-04 11:31:38 +0000

    Remove MappedList ctor and add TCExpressionList ctor

--- vdmj/src/main/java/com/fujitsu/vdmj/mapper/MappedList.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/mapper/MappedList.java
@@ -44,12 +44,6 @@ abstract public class MappedList<FROM extends Mappable, TO extends Mappable> ext
 		}
 	}
 	
-    public MappedList(List<TO> to)
-    {
-    	this();
-    	addAll(to);
-    }
-	
 	public MappedList()
 	{
 		super();
