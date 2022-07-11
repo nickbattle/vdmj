1c0b141ad32de7bbbd5c9f252f3146aab239cd4c
nick_battle <nick.battle@gmail.com>
2022-07-11 13:35:36 +0100

    Moved POMs to 4.4.5

--- lsp/src/main/scripts/lsp.sh
+++ lsp/src/main/scripts/lsp.sh
@@ -11,8 +11,8 @@ function usage()
     exit 1
 }
 
-MVERSION=4.4.5-SNAPSHOT
-PVERSION=4.4.5-P-SNAPSHOT
+MVERSION="4.4.5"
+PVERSION="4.4.5-P"
 VERSION=$MVERSION
 
 # The Maven repository directory containing jars
