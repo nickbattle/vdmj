81f8f04f6066a5d18f5e6a3c091e1faf74505c15
nick_battle <nick.battle@gmail.com>
2022-08-15 12:25:25 +0100

    Cleaned up simulation test

--- examples/simulation/src/main/java/simulation/TestSimulation.java
+++ examples/simulation/src/main/java/simulation/TestSimulation.java
@@ -28,13 +28,44 @@ import com.fujitsu.vdmj.RemoteSimulation;
 import com.fujitsu.vdmj.ast.definitions.ASTClassList;
 import com.fujitsu.vdmj.runtime.ValueException;
 
+/**
+ * This simple example is designed to run with the sporadic.vdmrt test spec in
+ * src/test/resources. It demonstrates setting parameters and changing the value
+ * of runtime system variables during a simulation.
+ * 
+ * Execute in VDM by adding the project to the classpath and passing:
+ * 
+ *     -simulation simulation.TestSimulation
+ * 
+ * Then execute new Test().test() as usual. The output should be like:
+ * 
+ * > p new Test().test()
+ * Last = 0
+ * Updated = 1
+ * Last = 1
+ * Updated = 2
+ * Last = 2
+ * Updated = 3
+ * Last = 3
+ * Updated = 4
+ * Last = 4
+ * Updated = 5
+ * Last = 5
+ * Updated = 6
+ * Last = 6
+ * Updated = 7
+ * = ()
+ * Executed in 0.057 secs.
+ */
 public class TestSimulation extends RemoteSimulation
 {
 	@Override
 	public void setup(ASTClassList classes)
 	{
 		setParameter(classes, "A", "MIN", 50);
+		System.out.println("Set MIN to 50");
 		setParameter(classes, "A", "MAX", 100);
+		System.out.println("Set MAX to 100");
 	}
 
 	@Override
