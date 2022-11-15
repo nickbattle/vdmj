1049a7f89a36f0da7aea35445ff402508e0e1677
nick_battle <nick.battle@gmail.com>
2022-11-15 12:48:14 +0000

    Added CSVReader example

--- examples/csvreader/README.md
+++ examples/csvreader/README.md
@@ -1,11 +1,11 @@
-## The ExternalFormatReader Example
+## CSV ExternalFormatReader Example
 
 This project contains a very simple example of how to use VDMJ external format parsers. These
 are used to extract VDM content for document formats that are not supported by VDMJ implicitly,
 for example from PDF files or ZIP files, or perhaps HTML.
 
-The example in this project uses the Linux "pdftotext" command to process PDF files and extract
-VDM source from between "%%VDM%%" markers. But any technique may be used to extract content.
+The example in this project parses CSV lines as a list of VDM expressions, returning a simple
+type schema for the data, plus a value of that type representing the file content.
 
 A reader class must implement the ExternalFormatReader interface. Its getText method must
 return the *entire* encapsulated VDM content from the document in one read operation - this
@@ -13,6 +13,5 @@ usually means the method must "preread" the content into a local buffer, like a 
 which is then used to generate a String's toCharArray().
 
 To plug the new reader (or multiple readers) into VDMJ, the "vdmj.parser.external_readers" property
-must be set to a CSV list of <suffix>=<class> pairs, like ".pdf=examples.parser.PDFStreamReader".
-This will attempt to use the example PDFStreamReader class for all files that end in ".pdf". Note
-that the suffix is not case sensitive.
+must be set to a CSV list of <suffix>=<class> pairs, like ".csv=examples.csvreader.CSVReader".
+Note that the suffix is not case sensitive.
---
+++ examples/csvreader/src/main/java/examples/csvreader/CSVReader.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2022 Nick Battle.
+ *
+ *	Author: Nick Battle
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *	SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ ******************************************************************************/
+
+package examples.csvreader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Date;
+
+import com.fujitsu.vdmj.ast.expressions.ASTExpressionList;
+import com.fujitsu.vdmj.lex.Dialect;
+import com.fujitsu.vdmj.lex.ExternalFormatReader;
+import com.fujitsu.vdmj.lex.LexTokenReader;
+import com.fujitsu.vdmj.messages.LocatedException;
+import com.fujitsu.vdmj.syntax.ExpressionReader;
+
+public class CSVReader implements ExternalFormatReader
+{
+	private static final String INDENT = "    ";
+
+	@Override
+	public char[] getText(File file, Charset charset) throws IOException
+	{
+		StringBuilder vdm = headers(file);
+		vdm.append(INDENT);
+		vdm.append("[");
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+		String sep = "";
+		
+		for (String line = reader.readLine(); line != null; line = reader.readLine())
+		{
+			vdm.append(sep);
+			vdm.append("\n");
+			vdm.append(INDENT);
+			vdm.append(INDENT);
+			vdm.append(csvLine(line));
+			sep = ",";
+		}
+		
+		vdm.append("\n");
+		vdm.append(INDENT);
+		vdm.append("];\n");
+		reader.close();
+		
+		return vdm.toString().toCharArray();
+	}
+	
+	private StringBuilder headers(File file)
+	{
+		StringBuilder vdm = new StringBuilder();
+
+		vdm.append("--\n");
+		vdm.append("-- Generated from ");
+		vdm.append(file.getPath());
+		vdm.append(" at ");
+		vdm.append(new Date().toString());
+		vdm.append("\n--\n");
+		vdm.append("types\n");
+		vdm.append(INDENT);
+		vdm.append("CSVCell = nat | seq of char | set of nat;\n");
+		vdm.append(INDENT);
+		vdm.append("CSVRow  = seq1 of CSVCell;\n");
+		vdm.append(INDENT);
+		vdm.append("CSVFile = seq of CSVRow;\n\n");
+
+		vdm.append("values\n");
+		vdm.append(INDENT);
+		vdm.append(System.getProperty("csvreader.name", nameFromFile(file)));
+		vdm.append(" : CSVFile =\n");
+		
+		return vdm;
+	}
+	
+	private String nameFromFile(File file)
+	{
+		String name = file.getName();
+		return name.replaceAll("\\.", "_");
+	}
+
+	private String csvLine(String line)
+	{
+		try
+		{
+			LexTokenReader ltr = new LexTokenReader(line, Dialect.VDM_SL);
+			ltr.nextToken();
+			ExpressionReader reader = new ExpressionReader(ltr);
+			ASTExpressionList list = reader.readExpressionList();
+			return "[ " + list.toString() + " ]";
+		}
+		catch (LocatedException e)
+		{
+			return "-- " + e.getMessage() + " at pos " + e.location.startPos + ": " + line;
+		}
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		CSVReader csv = new CSVReader();
+		System.out.println(new String(csv.getText(new File(args[0]), Charset.defaultCharset())));
+	}
+}
