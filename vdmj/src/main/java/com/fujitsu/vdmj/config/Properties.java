c4cf4957cc64225035532fd64db8f5638a25ef05
nick_battle <nick.battle@gmail.com>
2022-11-21 18:36:23 +0000

    Tweak type bind expansion limit to 100K

--- vdmj/src/main/java/com/fujitsu/vdmj/config/Properties.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/config/Properties.java
@@ -115,7 +115,7 @@ public class Properties
 	public static int in_powerset_limit = 30;
 	
 	/** The size limit for type bind expansions */
-	public static long in_typebind_limit = 1000000;
+	public static long in_typebind_limit = 100000;
 	
 	/**
 	 * When the class is initialized, which uses the vdmj.properties file, and any System
@@ -202,7 +202,7 @@ public class Properties
 		rt_diags_timestep = get(vdmj, "vdmj.rt.diags_timestep", false);
 		
 		in_powerset_limit = get(vdmj, "vdmj.in.powerset_limit", 30);
-		in_typebind_limit = get(vdmj, "vdmj.in.typebind_limit", 10000000);
+		in_typebind_limit = get(vdmj, "vdmj.in.typebind_limit", 1000000);
 
 		cmd_plugin_packages = get(vdmj, "vdmj.cmd.plugin_packages", "plugins");
 		debug_link_class = get(vdmj, "vdmj.debug.link_class", null);		
