e831762d8e45eca6c54c1037a3ed61c45290728f
nick_battle <nick.battle@gmail.com>
2022-11-23 09:51:04 +0000

    Add vdmj.in_typebind_limit to DAP manager list

--- lsp/src/main/java/workspace/DAPWorkspaceManager.java
+++ lsp/src/main/java/workspace/DAPWorkspaceManager.java
@@ -119,7 +119,8 @@ public class DAPWorkspaceManager
 		"vdmj.rt.max_periodic_overlaps",
 		"vdmj.rt.diags_guards",
 		"vdmj.rt.diags_timestep",
-		"vdmj.in.powerset_limit"
+		"vdmj.in.powerset_limit",
+		"vdmj.in_typebind_limit"
 	);
 	
 	protected DAPWorkspaceManager()
