41f72644e17a6b5e485c923b1b6c61b3e3442e47
nick_battle <nick.battle@gmail.com>
2023-02-07 19:12:09 +0000

    If long arithmetic operations overflow, try using doubles

--- vdmj/src/main/java/com/fujitsu/vdmj/in/expressions/INPlusExpression.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/in/expressions/INPlusExpression.java
@@ -53,17 +53,22 @@ public class INPlusExpression extends INNumericBinaryExpression
 
 			if (NumericValue.areIntegers(l, r))
 			{
-				long lv = l.intValue(ctxt);
-				long rv = r.intValue(ctxt);
-				long sum = addExact(lv, rv, ctxt);
-				return NumericValue.valueOf(sum, ctxt);
+				try
+				{
+					long lv = l.intValue(ctxt);
+					long rv = r.intValue(ctxt);
+					long sum = addExact(lv, rv, ctxt);
+					return NumericValue.valueOf(sum, ctxt);
+				}
+				catch (ValueException e)
+				{
+					// Too big... try reals
+				}
 			}
-			else
-			{
-				double lv = l.realValue(ctxt);
-				double rv = r.realValue(ctxt);
-	    		return NumericValue.valueOf(lv + rv, ctxt);
-			}
+
+			double lv = l.realValue(ctxt);
+			double rv = r.realValue(ctxt);
+    		return NumericValue.valueOf(lv + rv, ctxt);
     	}
     	catch (ValueException e)
     	{
