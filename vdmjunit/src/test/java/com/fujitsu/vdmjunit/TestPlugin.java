24f70ff918ccd07f1f56d1a466d34c64109751a2
nick_battle <nick.battle@gmail.com>
2023-02-15 09:37:53 +0000

    Added TestPlugin test for VDMJUnit

---
+++ vdmjunit/src/test/java/com/fujitsu/vdmjunit/TestPlugin.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2023 Nick Battle.
+ *
+ *	Author: Nick Battle
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *	SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ ******************************************************************************/
+
+package com.fujitsu.vdmjunit;
+
+import java.util.List;
+
+import com.fujitsu.vdmj.lex.Dialect;
+import com.fujitsu.vdmj.messages.VDMMessage;
+import com.fujitsu.vdmj.plugins.AnalysisEvent;
+import com.fujitsu.vdmj.plugins.AnalysisPlugin;
+import com.fujitsu.vdmj.plugins.EventListener;
+import com.fujitsu.vdmj.plugins.events.CheckPrepareEvent;
+
+public class TestPlugin extends AnalysisPlugin implements EventListener
+{
+	private int count = 0;
+	
+	public static AnalysisPlugin factory(Dialect dialect)
+	{
+		return new TestPlugin();
+	}
+
+	@Override
+	public String getName()
+	{
+		return "Test";
+	}
+
+	@Override
+	public void init()
+	{
+		eventhub.register(CheckPrepareEvent.class, this);
+	}
+
+	@Override
+	public List<VDMMessage> handleEvent(AnalysisEvent event) throws Exception
+	{
+		if (event instanceof CheckPrepareEvent)
+		{
+			CheckPrepareEvent cev = (CheckPrepareEvent)event;
+			count  = cev.getFiles().size();
+		}
+		
+		return null;
+	}
+	
+	public int getCount()
+	{
+		return count;
+	}
+}
