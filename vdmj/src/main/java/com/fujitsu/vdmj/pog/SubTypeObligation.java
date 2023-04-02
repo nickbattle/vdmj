04932885307e38303bde590666d51c070e2f3521
nick_battle <nick.battle@gmail.com>
2023-04-02 13:39:01 +0100

    Subtype PO cleanup

--- vdmj/src/main/java/com/fujitsu/vdmj/pog/SubTypeObligation.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/pog/SubTypeObligation.java
@@ -258,7 +258,7 @@ public class SubTypeObligation extends ProofObligation
 			TCInvariantType et = (TCInvariantType)etype;
 			prefix = "";
 
-			if (et.invdef != null)
+			if (et.invdef != null && !et.isMaximal())
 			{
 				TCNameToken invname = et.invdef.name;
 				
