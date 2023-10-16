7c50298d66d0e09f04d7f3e7f24809e79809270e
nick_battle <nick.battle@gmail.com>
2023-10-16 14:24:39 +0100

    Add -cmd option for running QC etc

--- vdmj/src/main/java/com/fujitsu/vdmj/plugins/analyses/INPlugin.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/plugins/analyses/INPlugin.java
@@ -76,7 +76,8 @@ abstract public class INPlugin extends AnalysisPlugin implements EventListener
 	protected boolean startInterpreter;		// eg. -e or -remote as well as -i
 	protected boolean interactive;			// eg. -i or -simulation
 	protected String defaultName;
-	protected String expression;
+	protected String expression;			// eg. -e "f(1, 2)"
+	protected String commandline;			// eg. -cmd "qc -s fixed"
 	protected String logfile;
 	protected String remoteControlName;
 	protected String remoteSimulationName;
@@ -136,6 +137,7 @@ abstract public class INPlugin extends AnalysisPlugin implements EventListener
 	{
 		println("-i: run the interpreter if successfully type checked");
 		println("-e <exp>: evaluate <exp> and stop");
+		println("-cmd <command>: perform <command> and stop");
 		println("-default <name>: set the default module/class");
 		println("-pre: disable precondition checks");
 		println("-post: disable postcondition checks");
@@ -177,6 +179,22 @@ abstract public class INPlugin extends AnalysisPlugin implements EventListener
 					else
 					{
 						fail("-e option requires an expression");
+					}
+					break;
+
+				case "-cmd":
+					iter.remove();
+					startInterpreter = true;
+					interactive = false;
+					
+					if (iter.hasNext())
+					{
+						commandline = iter.next();
+						iter.remove();
+					}
+					else
+					{
+						fail("-cmd option requires a command");
 					}
 					break;
 
