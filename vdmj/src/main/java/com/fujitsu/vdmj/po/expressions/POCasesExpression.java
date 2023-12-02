8957ef387ace129b5c0144046c8a079abf278454
Nick Battle <nick.battle@gmail.com>
2023-12-02 18:37:30 +0000

    Better hidden variable detection

--- vdmj/src/main/java/com/fujitsu/vdmj/po/expressions/POCasesExpression.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/po/expressions/POCasesExpression.java
@@ -69,6 +69,7 @@ public class POCasesExpression extends POExpression
 		int count = 0;
 		boolean hasIgnore = false;
 		TCNameList hidden = new TCNameList();
+		ProofObligationList _obligations = new ProofObligationList();
 
 		for (POCaseAlternative alt: cases)
 		{
@@ -80,20 +81,13 @@ public class POCasesExpression extends POExpression
 			hidden.addAll(alt.pattern.getHiddenVariables());	// cumulative
 			
 			// PONotCaseContext pushed by the POCaseAlternative...
-			ProofObligationList _obligations = alt.getProofObligations(ctxt, expType, env);
-
-			if (!hidden.isEmpty())
-			{
-				_obligations.markUnchecked("Obligation patterns contain hidden variables: " + hidden);
-			}
-
-			obligations.addAll(_obligations);
+			_obligations.addAll(alt.getProofObligations(ctxt, expType, env));
 			count++;
 		}
 		
 		if (others != null)
 		{
-			obligations.addAll(others.getProofObligations(ctxt, env));
+			_obligations.addAll(others.getProofObligations(ctxt, env));
 		}
 
 		for (int i=0; i<count; i++)
@@ -103,9 +97,16 @@ public class POCasesExpression extends POExpression
 
 		if (others == null && !hasIgnore)
 		{
-			obligations.add(new CasesExhaustiveObligation(this, ctxt));
+			_obligations.add(new CasesExhaustiveObligation(this, ctxt));
 		}
 
+
+		if (!hidden.isEmpty())
+		{
+			_obligations.markUnchecked("Obligation patterns contain hidden variables: " + hidden);
+		}
+
+		obligations.addAll(_obligations);
 		return obligations;
 	}
 
