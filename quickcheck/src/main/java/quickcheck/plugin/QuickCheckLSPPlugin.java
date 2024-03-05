f75c4a8573dc7694355e441766966f08e9a50856
Nick Battle <nick.battle@gmail.com>
2024-03-05 10:13:24 +0000

    Add handler for unknown QC event

--- quickcheck/src/main/java/quickcheck/plugin/QuickCheckLSPPlugin.java
+++ quickcheck/src/main/java/quickcheck/plugin/QuickCheckLSPPlugin.java
@@ -24,14 +24,23 @@
 
 package quickcheck.plugin;
 
+import java.util.Vector;
+
 import com.fujitsu.vdmj.lex.Dialect;
 import com.fujitsu.vdmj.plugins.HelpList;
 import com.fujitsu.vdmj.util.Utils;
 
+import dap.DAPMessageList;
+import dap.DAPRequest;
 import json.JSONObject;
+import quickcheck.QuickCheck;
+import quickcheck.commands.QCConsole;
 import quickcheck.commands.QCRunLSPCommand;
+import quickcheck.commands.QuickCheckExecutor;
 import quickcheck.commands.QuickCheckLSPCommand;
 import vdmj.commands.AnalysisCommand;
+import workspace.events.DAPEvent;
+import workspace.events.UnknownCommandEvent;
 import workspace.plugins.AnalysisPlugin;
 
 public class QuickCheckLSPPlugin extends AnalysisPlugin
@@ -53,6 +62,47 @@ public class QuickCheckLSPPlugin extends AnalysisPlugin
 		// Get everything from PO?
 	}
 	
+	@Override
+	public DAPMessageList handleEvent(DAPEvent event) throws Exception
+	{
+		if (event instanceof UnknownCommandEvent)
+		{
+			UnknownCommandEvent ume = (UnknownCommandEvent)event;
+			
+			switch (ume.request.getCommand())
+			{
+				case "slsp/POG/quickcheck":
+					return quickCheck(ume.request);
+			}
+		}
+		
+		return null;
+	}
+	
+	private DAPMessageList quickCheck(DAPRequest request)
+	{
+		QuickCheck qc = new QuickCheck();
+
+		qc.loadStrategies(new Vector<String>());	// Use defaults
+		
+		if (qc.hasErrors())
+		{
+			return new DAPMessageList(request, false, "Failed to load QC strategies", null);
+		}
+		
+		QCConsole.setQuiet(true);
+		QCConsole.setVerbose(false);
+
+		Vector<Integer> poList = new Vector<Integer>();
+		Vector<String> poNames = new Vector<String>();
+		poNames.add(".*");	// Include everything
+		
+		QuickCheckExecutor executor = new QuickCheckExecutor(request, qc, 1L, poList, poNames);
+		executor.start();
+		
+		return null;
+	}
+
 	@Override
 	public void setServerCapabilities(JSONObject capabilities)
 	{
