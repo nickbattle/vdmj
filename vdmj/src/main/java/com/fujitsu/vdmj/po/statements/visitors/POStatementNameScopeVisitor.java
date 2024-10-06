9008baec2a4a78eae4af3f8cce1b6e0794bf4b60
nick_battle <nick.battle@gmail.com>
2024-10-06 11:35:28 +0100

    Add name scope visitor framework

---
+++ vdmj/src/main/java/com/fujitsu/vdmj/po/statements/visitors/POStatementNameScopeVisitor.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2024 Nick Battle.
+ *
+ *	Author: Nick Battle
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *	SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ ******************************************************************************/
+
+package com.fujitsu.vdmj.po.statements.visitors;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fujitsu.vdmj.po.PONode;
+import com.fujitsu.vdmj.po.POVisitorSet;
+import com.fujitsu.vdmj.po.statements.POStatement;
+
+/**
+ * A visitor set to explore the PO tree and update the NameScopes accessed.
+ */
+public class POStatementNameScopeVisitor extends POLeafStatementVisitor<PONode, Set<PONode>, Object>
+{
+	public POStatementNameScopeVisitor(POVisitorSet<PONode, Set<PONode>, Object> visitors)
+	{
+		super(false);
+		this.visitorSet = visitors;
+	}
+	
+	@Override
+	protected Set<PONode> newCollection()
+	{
+		return new HashSet<PONode>();
+	}
+
+	@Override
+	public Set<PONode> caseStatement(POStatement node, Object arg)
+	{
+		return newCollection();
+	}
+}
