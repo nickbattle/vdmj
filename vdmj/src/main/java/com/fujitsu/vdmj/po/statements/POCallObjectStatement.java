fd559b64a03dfcc87bf6426b07acf45f2497465e
Nick Battle <nick.battle@gmail.com>
2024-11-08 11:16:45 +0000

    Add PO subtype checks for call arguments

--- vdmj/src/main/java/com/fujitsu/vdmj/po/statements/POCallObjectStatement.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/po/statements/POCallObjectStatement.java
@@ -26,15 +26,23 @@ package com.fujitsu.vdmj.po.statements;
 
 import com.fujitsu.vdmj.ast.lex.LexNameToken;
 import com.fujitsu.vdmj.po.definitions.PODefinition;
+import com.fujitsu.vdmj.po.definitions.POExplicitFunctionDefinition;
+import com.fujitsu.vdmj.po.definitions.POExplicitOperationDefinition;
+import com.fujitsu.vdmj.po.definitions.POImplicitFunctionDefinition;
+import com.fujitsu.vdmj.po.definitions.POImplicitOperationDefinition;
 import com.fujitsu.vdmj.po.expressions.POExpression;
 import com.fujitsu.vdmj.po.expressions.POExpressionList;
 import com.fujitsu.vdmj.po.statements.visitors.POStatementVisitor;
 import com.fujitsu.vdmj.pog.POContextStack;
 import com.fujitsu.vdmj.pog.POGState;
 import com.fujitsu.vdmj.pog.ProofObligationList;
+import com.fujitsu.vdmj.pog.SubTypeObligation;
 import com.fujitsu.vdmj.tc.lex.TCIdentifierToken;
 import com.fujitsu.vdmj.tc.lex.TCNameToken;
+import com.fujitsu.vdmj.tc.types.TCType;
+import com.fujitsu.vdmj.tc.types.TCTypeList;
 import com.fujitsu.vdmj.typechecker.Environment;
+import com.fujitsu.vdmj.typechecker.TypeComparator;
 import com.fujitsu.vdmj.util.Utils;
 
 public class POCallObjectStatement extends POStatement
@@ -72,15 +80,62 @@ public class POCallObjectStatement extends POStatement
 	public ProofObligationList getProofObligations(POContextStack ctxt, POGState pogState, Environment env)
 	{
 		ProofObligationList obligations = new ProofObligationList();
+		TCTypeList paramTypes = getParamTypes();
+		int i = 0;
 
 		for (POExpression exp: args)
 		{
 			obligations.addAll(exp.getProofObligations(ctxt, pogState, env));
+
+			TCType pt = paramTypes.get(i);
+			TCType at = exp.getExptype();
+			
+			if (!TypeComparator.isSubType(at, pt))
+			{
+				obligations.add(new SubTypeObligation(args.get(i), pt, at, ctxt));
+			}
+
+			i++;
 		}
 
 		pogState.addOperationCall(location, fdef);
 		
 		return obligations;
+	}
+
+	private TCTypeList getParamTypes()
+	{
+		if (fdef instanceof POExplicitOperationDefinition)
+		{
+			POExplicitOperationDefinition exop = (POExplicitOperationDefinition)fdef;
+			return exop.type.parameters;
+		}
+		else if (fdef instanceof POImplicitOperationDefinition)
+		{
+			POImplicitOperationDefinition imop = (POImplicitOperationDefinition)fdef;
+			return imop.type.parameters;
+		}
+		else if (fdef instanceof POExplicitFunctionDefinition)
+		{
+			POExplicitFunctionDefinition exfn = (POExplicitFunctionDefinition)fdef;
+			return exfn.type.parameters;
+		}
+		else if (fdef instanceof POImplicitFunctionDefinition)
+		{
+			POImplicitFunctionDefinition imfn = (POImplicitFunctionDefinition)fdef;
+			return imfn.type.parameters;
+		}
+		else	// Should never happen, but don't fail
+		{
+			TCTypeList list = new TCTypeList();
+			
+			for (POExpression arg: args)
+			{
+				list.add(arg.getExptype());
+			}
+			
+			return list;
+		}
 	}
 
 	@Override
