898b029297a0bd3c15819f2dd2df1141769dc0dd
Nick Battle <nick.battle@gmail.com>
2025-06-30 17:06:27 +0100

    Remove unused import

--- vdmj/src/main/java/com/fujitsu/vdmj/po/expressions/visitors/POExpressionStateUpdates.java
+++ vdmj/src/main/java/com/fujitsu/vdmj/po/expressions/visitors/POExpressionStateUpdates.java
@@ -29,7 +29,6 @@ import com.fujitsu.vdmj.po.expressions.POExpression;
 import com.fujitsu.vdmj.po.expressions.POVariableExpression;
 import com.fujitsu.vdmj.tc.lex.TCNameSet;
 import com.fujitsu.vdmj.tc.lex.TCNameToken;
-import com.fujitsu.vdmj.typechecker.NameScope;
 
 /**
  * A visitor set to explore the PO tree and return the state names updated.
